f98bb0a6ff8d600a40fa02bebc2c0b021333f333
Luka Juras <mrfsrf@gmail.com>
2025-01-14 11:57:57 +0100

    Fix: one of the test fails

--- src/Image/Operation/ToWebp.php
+++ src/Image/Operation/ToWebp.php
@@ -5,6 +5,7 @@ namespace Timber\Image\Operation;
 use Timber\Image\Converter\CWebPConverter;
 use Timber\Image\Converter\GDConverter;
 use Timber\Image\Converter\ImagickConverter;
+use Timber\Image\Converter\IConverter;
 use Timber\Image\Operation as ImageOperation;
 use Timber\ImageHelper;
 
